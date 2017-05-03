97e6b44ca8d8fef48872eb5a9fabb860c34baac7
Prativa <devkotaprativa@gmail.com>
2017-05-03 18:40:31 -0500

    relation between jobs model and categories model defined.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170503231335) do
+ActiveRecord::Schema.define(version: 20170503231624) do
 
   create_table "categories", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string   "name"
@@ -25,6 +25,7 @@ ActiveRecord::Schema.define(version: 20170503231335) do
     t.string   "url"
     t.datetime "created_at",                null: false
     t.datetime "updated_at",                null: false
+    t.integer  "category_id"
   end
 
 end
